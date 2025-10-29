259e738f5e8a738907cdb8cdbed8306f2e3d5dce
Gaoss97 <133462273+Gaoss97@users.noreply.github.com>
2025-10-29 18:17:39 -0300

    Update gpio.c

--- tiva/gpio.c
+++ tiva/gpio.c
@@ -8,73 +8,96 @@
 
 #include "tm4c1294ncpdt.h"
 
-  
+#define GPIO_PORTA  (0x0001) //bit 0
 #define GPIO_PORTJ  (0x0100) //bit 8
 #define GPIO_PORTK  (0x200)  //bit 9
+#define GPIO_PORTL  (0x400)  //bit 10
 #define GPIO_PORTN  (0x1000) //bit 12
 #define GPIO_PORTM  (0x800)  //bit 11
+#define GPIO_PORTQ  (0x4000) //bit 14
 
 // -------------------------------------------------------------------------------
-// FunÁ„o GPIO_Init
+// Fun√ß√£o GPIO_Init
 // Inicializa os ports J e N
-// Par‚metro de entrada: N„o tem
-// Par‚metro de saÌda: N„o tem
+// Par√¢metro de entrada: N√£o tem
+// Par√¢metro de sa√≠da: N√£o tem
 void GPIO_Init(void)
 {
 	//1a. Ativar o clock para a porta setando o bit correspondente no registrador RCGCGPIO
-	SYSCTL_RCGCGPIO_R = (GPIO_PORTJ | GPIO_PORTN);
-	//1b.   apÛs isso verificar no PRGPIO se a porta est· pronta para uso.
-  while((SYSCTL_PRGPIO_R & (GPIO_PORTJ | GPIO_PORTN) ) != (GPIO_PORTJ | GPIO_PORTN) ){};
+	SYSCTL_RCGCGPIO_R = (GPIO_PORTJ | GPIO_PORTN | GPIO_PORTA | GPIO_PORTQ | GPIO_PORTM | GPIO_PORTL);
+	//1b.   ap√≥s isso verificar no PRGPIO se a porta est√° pronta para uso.
+  while((SYSCTL_PRGPIO_R & (GPIO_PORTJ | GPIO_PORTN | GPIO_PORTA | GPIO_PORTQ | GPIO_PORTM | GPIO_PORTL) ) != (GPIO_PORTJ | GPIO_PORTN | GPIO_PORTA | GPIO_PORTQ | GPIO_PORTM | GPIO_PORTL) ){};
 	
-	// 2. Limpar o AMSEL para desabilitar a analÛgica
+	// 2. Limpar o AMSEL para desabilitar a anal√≥gica
 	GPIO_PORTJ_AHB_AMSEL_R = 0x00;
 	GPIO_PORTN_AMSEL_R = 0x00;
+	GPIO_PORTQ_AMSEL_R = 0x00;
+	GPIO_PORTA_AHB_AMSEL_R = 0x00;
+  GPIO_PORTL_AMSEL_R = 0x00;
+	GPIO_PORTM_AMSEL_R = 0x00;
 		
 	// 3. Limpar PCTL para selecionar o GPIO
 	GPIO_PORTJ_AHB_PCTL_R = 0x00;
 	GPIO_PORTN_PCTL_R = 0x00;
+	GPIO_PORTA_AHB_PCTL_R = 0x00;
+	GPIO_PORTQ_PCTL_R = 0x00;
+	GPIO_PORTM_PCTL_R = 0x00;
+	GPIO_PORTL_PCTL_R = 0x00;
 
-	// 4. DIR para 0 se for entrada, 1 se for saÌda
+	// 4. DIR para 0 se for entrada, 1 se for sa√≠da
 	GPIO_PORTJ_AHB_DIR_R = 0x00;
 	GPIO_PORTN_DIR_R = 0x03; //BIT0 | BIT1
+	GPIO_PORTA_AHB_DIR_R = 0x0F;
+	GPIO_PORTQ_DIR_R = 0x0F;
+	GPIO_PORTM_DIR_R = 0x07;
+	GPIO_PORTL_DIR_R = 0x00;
 		
-	// 5. Limpar os bits AFSEL para 0 para selecionar GPIO sem funÁ„o alternativa	
+	// 5. Limpar os bits AFSEL para 0 para selecionar GPIO sem fun√ß√£o alternativa	
 	GPIO_PORTJ_AHB_AFSEL_R = 0x00;
-	GPIO_PORTN_AFSEL_R = 0x00; 
-		
+	GPIO_PORTN_AFSEL_R = 0x00;
+	GPIO_PORTA_AHB_AFSEL_R = 0x00;
+	GPIO_PORTQ_AFSEL_R = 0x00; 	
+	GPIO_PORTM_AFSEL_R = 0x00;
+	GPIO_PORTL_AFSEL_R = 0x00;
+	
 	// 6. Setar os bits de DEN para habilitar I/O digital	
 	GPIO_PORTJ_AHB_DEN_R = 0x03;   //Bit0 e bit1
 	GPIO_PORTN_DEN_R = 0x03; 		   //Bit0 e bit1
+	GPIO_PORTA_AHB_DEN_R = 0x0F;   //Bit0 e bit1
+	GPIO_PORTQ_DEN_R = 0x0F;			 //Bit0 e bit1
+	GPIO_PORTM_DEN_R = 0xF7;
+	GPIO_PORTL_DEN_R = 0x0F;
 	
 	// 7. Habilitar resistor de pull-up interno, setar PUR para 1
-	GPIO_PORTJ_AHB_PUR_R = 0x03;   //Bit0 e bit1	
+	GPIO_PORTJ_AHB_PUR_R = 0x03;	//Bit0 e bit1
+	GPIO_PORTL_PUR_R = 0x0F;
 	
 	
 
 }	
 
 // -------------------------------------------------------------------------------
-// FunÁ„o PortJ_Input
-// LÍ os valores de entrada do port J
-// Par‚metro de entrada: N„o tem
-// Par‚metro de saÌda: o valor da leitura do port
+// Fun√ß√£o PortJ_Input
+// L√™ os valores de entrada do port J
+// Par√¢metro de entrada: N√£o tem
+// Par√¢metro de sa√≠da: o valor da leitura do port
 uint32_t PortJ_Input(void)
 {
 	return GPIO_PORTJ_AHB_DATA_R;
 }
 
 // -------------------------------------------------------------------------------
-// FunÁ„o PortN_Output
+// Fun√ß√£o PortN_Output
 // Escreve os valores no port N
-// Par‚metro de entrada: Valor a ser escrito
-// Par‚metro de saÌda: n„o tem
+// Par√¢metro de entrada: Valor a ser escrito
+// Par√¢metro de sa√≠da: n√£o tem
 void PortN_Output(uint32_t valor)
 {
     uint32_t temp;
     //vamos zerar somente os bits menos significativos
-    //para uma escrita amig·vel nos bits 0 e 1
+    //para uma escrita amig√°vel nos bits 0 e 1
     temp = GPIO_PORTN_DATA_R & 0xFC;
-    //agora vamos fazer o OR com o valor recebido na funÁ„o
+    //agora vamos fazer o OR com o valor recebido na fun√ß√£o
     temp = temp | valor;
     GPIO_PORTN_DATA_R = temp; 
 }
